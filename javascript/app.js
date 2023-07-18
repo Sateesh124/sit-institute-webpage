c3ce30b31ffb10c4f0c397ef4e0f993855f176ea
Sateesh124 <115435751+Sateesh124@users.noreply.github.com>
2023-07-18 13:05:31 +0530

    santosh education project completed

---
+++ javascript/app.js
@@ -0,0 +1,46 @@
+
+// DOM 
+
+
+function init(){
+    const hamburger =document.querySelector(".hamburger");
+    const sidebarCloseButton = document.querySelector(".close-button")
+    const sidebarContainer =document.querySelector('.sidebar-container');
+    const openSidebar = () => {
+        sidebarContainer.classList.add("show-sidebar");
+      
+    };
+    hamburger.addEventListener("click",openSidebar);
+    const closeSidebar =() => {
+        sidebarContainer.classList.remove('show-sidebar');
+            
+   };
+   hamburger.addEventListener("click",openSidebar);
+   sidebarContainer.addEventListener("click",closeSidebar);
+
+}
+
+init();
+
+
+
+
+function navRelatedCode() {
+    const navbarContainer = document.querySelector(".nav-container");
+window.addEventListener("scroll",function(){
+    
+    const scrollNumber = window.scrollY;
+    const targetNumber =  window.innerWidth >=992 ? 70 : 50;
+    if (scrollNumber > targetNumber) {
+        navbarContainer.classList.add('sticky-nav');
+    }
+    else {
+        navbarContainer.classList.remove('sticky-nav');
+    }
+});
+
+
+}
+
+navRelatedCode();
+
